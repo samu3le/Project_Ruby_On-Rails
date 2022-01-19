3e42ea8edc42f6edeb83aee3b7e2a9b2d0c23c48
Samuel Espinoza <samuelesponline@gmail.com>
2022-01-19 00:41:26 -0500

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,31 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
-        @article = Article.new  #variable con @ es global
-        @article.title = "Demo"
+        @article = Article.new  #variable con @ es global   
     end
 
     def create
-        @article = Article.create(title: params[:article][:title])
+        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
         render json: @article
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        redirect_to @article
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to root_path
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  get "bienvenida", to: "home#index"
-  # root to: "home#index" 
+  # get "bienvenida", to: "home#index"
+  root to: "home#index" 
 
   get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+
+  patch "articles/:id", to: "articles#update", as: :article
   post "articles", to: "articles#create"
+  delete "articles/:id", to: "articles#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
